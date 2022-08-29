e931d93e0d79422486460203f2098591d4747eee
ReinaArionAlkira <Renatis.wes00@gmail.com>
2022-08-29 20:03:16 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+//Day 1 - Measurements
+Console.WriteLine(Day1(1));
+Console.WriteLine(Day1(2));
+
+
+int Day1(int part)
+{
+    int diff = 0;
+    int amount = 0;
+    if (part == 1) diff = 1;
+    else if (part == 2) diff = 3;
+    else Console.WriteLine("Error");
+
+    string fileName = "input.txt";
+    string path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Data\", fileName));
+    //Console.WriteLine(path);
+
+    int[] array = File.ReadLines(path).Where(line => !string.IsNullOrEmpty(line)).Select(line => int.Parse(line)).ToArray();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0 || (diff == 3 && i < 3)) continue;
+        else if (array[i] > array[i - diff]) amount++;
+    }
+    return amount;
+}
